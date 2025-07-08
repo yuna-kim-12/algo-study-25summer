59df8892ad1fe8d05f19d1b6ddb8c32dcdc21e57
JE LEE <dlwldms050@naver.com>
2025-07-08 23:20:56 +0900

    update Programmers 43163

---
+++ JIEUN_L/Programmers_43163.py
@@ -0,0 +1,30 @@
+# dfs로 굴려보자! 
+
+# 1. for문으로 target 단어가 있는지 검사
+# 1.1 만일 있다면 DFS 함수 실행, 없다면 그대로 0을 리턴함
+
+# 2. FOR문에서 단어가 있는 경우 
+# 2.1 재귀 함수로 부름. 
+
+# 재귀함수(dfs)
+# VISITED, WORDS, INDEX, cnt 를 넘겨줌
+# 종료 조건 : 글자를 찾았다면 CNT를 글로벌로 넘기고 종료
+# 종료 조건 : 만일 CNT 가 WORDS의 갯수만큼 돌았는데 못갔으면 걍 0 리턴
+# FOR문에서 단어의 글자 한개만 변경되었으면서, 방문하지 않았던 경우 Dfs 호출
+# FOR문에서 만일 VISITED가 아니면 dfs호출하면서, 해당 위치를 vISITED 처리 & index & cnt +1를 변수 넘겨줌
+# 만일 FOR문을 돌았는데 방문할 곳이 없다면 0 리턴.
+
+count = 60
+
+def DFS(target, current, len_word, len_wordlist,visited, words, index, cnt) : 
+    if current == target : 
+        count = min(count, cnt)
+        return 
+    if cnt == len_wordlist : 
+        count = 0
+        return 
+    
+    wrong = 0
+    for 
+        for i in range(len_word) : 
+            current[i] != words[index][i]
