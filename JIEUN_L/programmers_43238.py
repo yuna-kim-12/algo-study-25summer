8d40fc80e63c8afdb28e7251f6d69197fa27d34a
JE LEE <dlwldms050@naver.com>
2025-07-20 21:56:09 +0900

    changing 43238

--- JIEUN_L/programmers_43238.py
+++ JIEUN_L/programmers_43238.py
@@ -1,13 +1,8 @@
-# 근데 이거 꼭 이진탐색이어야 함? 이거 그냥 비율로 하면 될 것 같은데... 
-
-# 1. a:b만큼의 시간이 차이가 나면 b:a비율로 인원수를 나누어가지는게 맞음.
-# 2. 하지만 b:a로 나눠 가진다고 정수가 나올 가능성이 낮으니, 일단 아래 수식대로 계산. 소숫점 떄문에 미리 계산은 금물. 
-# round(nb/(a+b)) : round(na/(a+b))
-# 여기서 a+b = n인지 확인하고, n보다 작다면a에 하나를 추가하고, n보다 크다면 b에서 하나를 뺸다.
-# 3. 일단 저 round후 int 로 바꿔서 일단 정수로 바꾸지만,소숫점 때문에 틀릴 수도 있으니 한가지 추가 작업을 더 함. a-1, a+1 의 경우에 대해서도 계산을 해서최소를 재확인 하는 것. 
+# 걍 쉬운 방법써봐야지 하.....
 
 
-
+# 1. 이전에 비율로 짠 코드의 인원수로 시작한다. 
+# 2. 비율로 최적해 비슷한걸 찾고, 양쪽으로 본다.
 
 def solution(n, times):
     answer = 0
@@ -31,16 +26,19 @@ def solution(n, times):
 
     # 일단은 갯수는 맞춰줌.
 
-    minus_1,plus_1 = 0, 0
+    # minus_1,plus_1 = 0, 0
 
     answer = max(times[0]*counter_1, times[1]*counter_2)
-    minus_1 = max(times[0]*(counter_1-1) , times[1]*(counter_2+1))
-    plus_1 = max(times[0]*(counter_1+1), times[1]*(counter_2-1))
+    # minus_1 = max(times[0]*(counter_1-1) , times[1]*(counter_2+1))
+    # plus_1 = max(times[0]*(counter_1+1), times[1]*(counter_2-1))
 
     # print(answer, minus_1, plus_1)
 
+    # 여기서 이제 양쪽으로 탐색해가는거지.
+    
+    while counter_1 >= 2  : 
+        pass
+        
 
-    return min(answer, minus_1, plus_1)
 
-
-print(solution(6, [7, 10]))
+    # return min(answer, minus_1, plus_1)
