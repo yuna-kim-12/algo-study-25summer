57e9368b9a829e44b811d7c584dc62909856ac7b
JE LEE <dlwldms050@naver.com>
2025-07-23 23:43:04 +0900

    42577

---
+++ JIEUN_L/programmers_42577.py
@@ -0,0 +1,11 @@
+# sort - 길이별로. 
+# 길이별로는 어떻게? 
+# startswith 를 쓰면 되는데.... 
+# 꼭 n^2을 해야 할까./....
+
+def solution(phone_book):
+    answer = True
+    
+    for i in range(len(phone_book)) : 
+        phone_book[i]
+    return answer
