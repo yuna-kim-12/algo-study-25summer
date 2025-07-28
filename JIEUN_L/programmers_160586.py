cec9fc4799dd87d48993ade0b12c50361d021c2b
JE LEE <dlwldms050@naver.com>
2025-07-29 01:01:56 +0900

    programmers 160586, updated logic

---
+++ JIEUN_L/programmers_160586.py
@@ -0,0 +1,10 @@
+# 아이디어
+# 1. 일단 A~Z 까지 전부 다 배열로 몇번째 키((최소))에 있는지 확인. A~Z는 전부 아스키코드로 ord인가? 그걸로 보자.
+# a 그리고 for문으로 보면서 키맵이 0이면 리턴 -1하는걸로.
+
+
+
+
+def solution(keymap, targets):
+    answer = []
+    return answer
