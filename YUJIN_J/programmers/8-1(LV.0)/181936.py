3f45a90dd774a83f366b8a930f81d8eec9210afe
jungyujin828 <tntn6981@naver.com>
2025-08-05 11:03:55 +0000

    Aug W1 - LV.0 문제 풀이

---
+++ YUJIN_J/programmers/8-1(LV.0)/181936.py
@@ -0,0 +1,12 @@
+# 공배수
+# 문제설명
+# 정수 number와 n, m이 주어집니다. number가 n의 배수이면서 m의 배수이면 1을 아니라면 0을 return하도록 solution 함수를 완성해주세요.
+
+# 느낀잠: 어려움 없이 풀었음
+
+def solution(number, n, m):
+    if number%n==0 and number%m==0:
+        answer = 1
+    else:
+        answer = 0
+    return answer
