7615aae279075e50ef9f81c4f2bf96215a5e6042
Jaidy0 <hyjgood2000@naver.com>
2025-08-06 16:36:28 +0900

    250806 : Java 1문 완료. 쉽지 않았다...

--- YEONJU_H/Java/SWEA/D250805SWEA5648/Solution.java
+++ YEONJU_H/Java/SWEA/D250805SWEA5648/Solution.java
@@ -6,7 +6,6 @@ import java.util.*;
 
 class Atom {
     int x, y, dir, energy;
-
     Atom(int x, int y, int dir, int energy) {
         this.x = x;
         this.y = y;
@@ -15,13 +14,14 @@ class Atom {
     }
 }
 
-
 public class Solution {
-    static List<Atom> atoms;
+    static Queue<Atom> atoms;
     static int N;
-    static int[] dx = {0,0,-1,1};
-    static int[] dy = {1,-1,0,0};
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {1, -1, 0, 0};
+    static int[][] map;
     static int crachScore;
+
     public static void main(String[] args) throws IOException {
 
         /*  문제 요약
@@ -44,6 +44,7 @@ public class Solution {
             - 방향 바뀌지 않음 / 방출 에너지 주변 영향 안줌
 
             [ 아이디어 ]
+            1. 첫 시도
             1) 1초씩 마다 1만큼 원자들 이동 시키기
             2) 이동 후, 충돌된 원자들 체크
             - x,y 값이 둘다 같은 원자 체크
@@ -52,7 +53,6 @@ public class Solution {
               충돌 후에는 해당 원자 제거
               -- 사분면 별로 충돌 로직 구현 필요
               서로 반대방향이면서 마주 볼 경우 / 상하좌우 0123
-              y,x /
               1) 방향이 서로 반대인 것끼리 체크. (한 원자를 잡고, 상이면 하만 필터링해서 본다.)
               상 : 하인 원자들 중, x값이 같고, y값이 위쪽위치이면 충돌
               하 : 상인 원자들 중, x값이 같고, y값이 아래쪽위치이면 충돌
@@ -60,6 +60,13 @@ public class Solution {
               우 : 좌인 원자들 중, y값이 같고, x값이 오른쪽위치이면 충돌
 
             3) 이 과정을 좌표값이 가장 큰 원자의 좌표값 기준 횟수만큼 반복
+            충돌을 체크하는 과정이 시간복잡도 N^2 라서, 시간 초과 발생
+
+
+            2. 두 번째 시도 : BFS 활용
+            시간차를 두는 것이 아니라, 원자를 순차적으로 pop하고 이동시키고 넣음으로써
+            순서대로 돌면서 충돌 확인하는 로직을 2차원 배열을 중개로 구현.
+            시간 복잡도 N으로 효율성 Up.
 
          */
 
@@ -67,102 +74,58 @@ public class Solution {
         int T = Integer.parseInt(br.readLine());
         int t = 0;
 
-        while(t++ < T) {
+        while (t++ < T) {
 
             N = Integer.parseInt(br.readLine());
-            atoms = new ArrayList<>();
+            atoms = new LinkedList<>();
             crachScore = 0;
+            map = new int[4001][4001];
 
-            int maxVal = 0;
-
-            for(int i = 0; i < N; i++) {
+            for (int i = 0; i < N; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
-                int x = Integer.parseInt(st.nextToken()) * 2;
-                int y = Integer.parseInt(st.nextToken()) * 2;
+                int x = (Integer.parseInt(st.nextToken()) + 1000) * 2;
+                int y = (Integer.parseInt(st.nextToken()) + 1000) * 2;
                 int dir = Integer.parseInt(st.nextToken());
                 int e = Integer.parseInt(st.nextToken());
-                atoms.add(new Atom(x,y,dir,e));
-
-                int curMaxVal = Math.max(Math.abs(x),Math.abs(y));
-                if(curMaxVal > maxVal) {
-                    maxVal = curMaxVal;
-                }
-
+                atoms.add(new Atom(x, y, dir, e));
+                map[x][y] = e;
             }
 
-            int n = 0;
-            while(n <= maxVal * 2 && !atoms.isEmpty()) {
-                moveAtoms();
-                checkCrash();
-                n++;
-                if(atoms.size() < 2) break;
-            }
-
+            bfs();
             System.out.println("#" + t + " " + crachScore);
         }
 
 
     }
 
-    public static void moveAtoms() {
+    public static void bfs() {
         // 모두 이동 시킴
-        for( Atom a : atoms) {
-            a.x += dx[a.dir];
-            a.y += dy[a.dir];
-        }
-    }
 
-    public static void checkCrash() {
-        boolean[] crashed = new boolean[atoms.size()];
+        while(!atoms.isEmpty()) {
+            Atom a = atoms.poll();
+            if(map[a.x][a.y] != a.energy) { // 다른 원자와 충돌 확인
+                crachScore += map[a.x][a.y];
+                map[a.x][a.y] = 0;
+                continue;
+            }
 
-        // 충돌여부 일괄 체크
-        for(int i = 0; i < atoms.size(); i++) {
-            if(crashed[i]) continue; // i가 이미 check 된거면 pass
-            Atom a1 = atoms.get(i);
-            for(int j = i + 1; j < atoms.size(); j++) {
-                Atom a2 = atoms.get(j);
-//                switch(a1.dir) {
-//                    case 0 :
-//                        if(a2.dir == 1 && a1.x == a2.x && a1.y < a2.y) {
-//                            crashed[i] = true;
-//                            crashed[j] = true;
-//                        }
-//                        break;
-//                    case 1:
-//                        if(a2.dir == 0 && a1.x == a2.x && a1.y > a2.y) {
-//                            crashed[i] = true;
-//                            crashed[j] = true;
-//                        }
-//                        break;
-//                    case 2:
-//                        if(a2.dir == 3 && a1.y == a2.y && a1.x < a2.x) {
-//                            crashed[i] = true;
-//                            crashed[j] = true;
-//                        }
-//                        break;
-//                    case 3:
-//                        if(a2.dir == 2 && a1.y == a2.y && a1.x > a2.x) {
-//                            crashed[i] = true;
-//                            crashed[j] = true;
-//                        }
-//                        break;
-//                }
+            int nx = a.x + dx[a.dir];
+            int ny = a.y + dy[a.dir];
 
-                if(a1.x == a2.x && a1.y == a2.y && !crashed[j]) {
-                    crashed[i] = true;
-                    crashed[j] = true;
+            if(nx >= 0 && ny >= 0 && nx <= 4000 && ny <= 4000) {
+                if (map[nx][ny] == 0) {
+                    map[nx][ny] = a.energy;
+                    atoms.add(new Atom(nx, ny, a.dir, a.energy));
+                } else {
+                    map[nx][ny] += a.energy;
                 }
             }
-        }
 
-        // 충돌된 원자 일괄 제거
-        for(int i = atoms.size() - 1; i >= 0; i--) {
-            if(crashed[i]) {
-                crachScore += atoms.get(i).energy;
-                atoms.remove(i);
-            }
+            map[a.x][a.y] = 0;
+
         }
 
     }
+
 
 }
