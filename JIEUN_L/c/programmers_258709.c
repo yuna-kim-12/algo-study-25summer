4ed5700e716f2e6a8f386a9c7fc398db78f5a7fa
JE LEE <dlwldms050@naver.com>
2025-08-18 00:24:52 +0900

    updated 258709 again.

--- JIEUN_L/c/programmers_258709.c
+++ JIEUN_L/c/programmers_258709.c
@@ -6,19 +6,20 @@
 // 1.일단 expectation이 가장 높은 쪽의 숫자를 고르는게 맞다. 
 // 2. 따라서 for 문으로 가장 exp 를 게산하도록 하자! 
 
-int compare(const void* a, const void* b)
+int compare(const void* a, const void* b);
 
 // dice_rows는 2차원 배열 dice의 행 길이, dice_cols는 2차원 배열 dice의 열 길이입니다.
 int* solution(int** dice, size_t dice_rows, size_t dice_cols) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
-    int* answer = (int*)malloc(sizeof(int)*dice_rows);
+    int* answer = (int*)malloc(sizeof(int)*dice_rows); // 일단 일케... 일케 잠시만 두장
     // int temp_arr[dice_rows][2];
     int **temp_arr = malloc(sizeof(int)*dice_rows);
     for(int i=0; i<dice_rows; i++){
-        temp_arr[i]= malloc(sizeof(int)*dice_cols);
+        
     }
     // expectation 찾기 
     for(int i=0; i<dice_rows; i++){
+        temp_arr[i]= malloc(sizeof(int)*dice_cols);
         int temp_sum = 0;
         for(int j=0; j<dice_cols; j++){
             temp_sum += dice[i][j];
@@ -30,7 +31,7 @@ int* solution(int** dice, size_t dice_rows, size_t dice_cols) {
         temp_arr[i][1] = i;
     }
     // 이제 여기서 q sort 를 하면 될듯?
-    qsort(temp_arr, dice_rows, sizeof(int*),  )
+    qsort(temp_arr, dice_rows, sizeof(int*),compare)
 
     return answer;
 }
@@ -51,4 +52,11 @@ int main(){
     printf("%d",*result);
     return 0;
 
+}
+
+int compare(const void* a, const void*b){
+    int x = *(const int)a;
+    int y = *(const int)b;
+
+    return (x>y) - (x<y);
 }
